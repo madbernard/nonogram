88ab999c9c0a16077e0c5d3ded295b8a6384fb01
Mad Bernard <madeline.a.bernard@gmail.com>
2019-03-24 23:23:57 -0700

    Add makeClues function

--- nonogram.js
+++ nonogram.js
@@ -28,10 +28,32 @@ function createNonogram (size) {
   return board;
 }
 
+// ///////////////// solving functions ///////////////// //
+// clues should be a tuple array of column clues, then row clues
+// so like [[[3],[1,2],[1],[2,1]], [[4],[1,1],[2],[1,1]]] for the below
+/*
+  1111
+  1001
+  1100
+  0101
+*/
+function solveNonogram (clues) {
+  var picture = '';
+  /*
+    basic algo is, clue says that in this line of x long there are groups, y, z, ... .
+    known space is then
+      y + z + ... + (length of group array minus 1 (for single space between each group) )
+    take known space, compare to line length, amount line is greater than known is l (for what the groups Lack)
+    place groups on line as if they started at 0, but do not place marks in the first l spaces
+    (this may break down with more groups than 2)
+  */
+  return picture;
+}
+
 // ///////////////// helper functions ///////////////// //
 
 // biasTowardsFilled is, like .1 if you want 60% of the spaces to be filled in
-// the more empty a nonogram is, the harder it is to solve
+// ...the more empty a nonogram is, the harder it is to solve
 function getRandomFiller (biasTowardsFilled) {
   return Math.round(Math.random() + biasTowardsFilled)
 }
@@ -44,7 +66,7 @@ function arraySum (arr) {
 
 console.log(arraySum([2,3]));
 
-var given = createNonogram(10);
+var given = createNonogram(5);
 
 // ///////////////// knowledge about nonogram ///////////////// //
 
@@ -52,32 +74,89 @@ var given = createNonogram(10);
   11 12 13
   21 22 23
   31 32 33
-  r#c#
+  row# then column#
 */
 function getGroupsInfo (nonogram) {
   var rows = nonogram.split('\n');
   console.log(rows);
+  var cols = [];
+  var n = 0;
 
-  var cols = rows.forEach(function(eachRow, colNum);{
-    for (var rowNum = 0; rowNum < eachRow.length; rowNum++) {
-      eachRow[colNum];
+  while (n < rows.length) {
+    for (var i = 0; i < rows.length; i++) {
+      rows[i]
     }
-  });
+    cols[0] += rows[0][0];
+    cols[1] += rows[1][0];
+    cols[2] += rows[2][0]
+    n++;
+  }
 }
 
 getGroupsInfo(given);
 
+function makeClues (nonogram) {
+  var cluesObj = {rowClues: [], colClues: []};
+  var rows = nonogram.split('\n');
+  var rowGroup, colGroup;
 
+  // we get a number, use it to choose row and col
+  for (var i = 0; i < rows.length; i++) {
+    rowGroup = 0;
+    colGroup = 0;
+    cluesObj.rowClues.push([]);
+    cluesObj.colClues.push([]);
+
+    // cycle past every point on row and in col
+    for (var cyc = 0; cyc < rows.length; cyc++) {
+      // coerce '0' to false
+      if (rows[i][cyc] == true) {
+        rowGroup++;
+      }
+      else if (rowGroup) {
+        cluesObj.rowClues[i].push(rowGroup);
+        rowGroup = 0;
+      }
+
+      // i goes up one each cycle
+      // cycle goes 0-x each cycle
+      if (rows[cyc][i] == true) {
+        colGroup++;
+      }
+      else if (colGroup) {
+        cluesObj.colClues[i].push(colGroup);
+        colGroup = 0;
+      }
+    }
+
+    // row 1 & col 1 are mapped, move on
+    // push stuff in group counters, and if arr has no length push 0
+    if (rowGroup) {cluesObj.rowClues[i].push(rowGroup);}
+    if (!cluesObj.rowClues[i].length) {cluesObj.rowClues[i].push(0);}
+    if (colGroup) {cluesObj.colClues[i].push(colGroup);}
+    if (!cluesObj.colClues[i].length) {cluesObj.colClues[i].push(0);}
+  }
+
+  console.log(cluesObj);
+  return cluesObj;
+}
+makeClues(given);
+
+// so like [[[3],[1,2],[1],[2,1]], [[4],[1,1],[2],[1,1]]] for the below
+/*
+  1111
+  1001
+  1100
+  0101
+*/
 
 // ///////////////// solving ///////////////// //
 /*
   If open space in line === unplaced spaces known... done
   if any unplaced group length > slop (open space - unplaced spaces known)... know (group - slop) spots in middle of open space... so, like, in an open space of 7, with a group of 5, you know 3 locations
 
+  object, r1 = {11: 'odds', 12: 'odds', 13: 'odds', solved: false}
 
-  rose
-
-MFOKACDuIpp
 
 
 */
